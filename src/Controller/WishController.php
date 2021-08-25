74e46c6e91983fc09e7db496c26022cc78f0baa6
KevinBTSsio <kev17.haramendy@gmail.com>
2021-08-25 16:02:06 +0200

    Ajout formulaire add wish

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Wish;
+use App\Form\WishType;
 use App\Repository\WishRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,17 +18,37 @@ class WishController extends AbstractController
      */
     public function wishlist(WishRepository $wr): Response
     {
-        $wishes = $wr->findAll();
+        $wishes = $wr->findBy(["isPublished" => true],["dateCreated" => "DESC"]);
         return $this->render('wish/list.html.twig', [
             "wishes" => $wishes
         ]);
     }
 
     /**
-     * @Route("/detail_wish", name="detail")
+     * @Route("/detail_wish/{id}", name="detail")
      */
-    public function detail($id, WishRepository $wr): Response
+    public function detail(): Response
     {
         return $this->render('wish/detail.html.twig');
+    }
+
+    /**
+     * @Route("/addWish", name="add_wish")
+     */
+    public function add_wish(Request $req, EntityManagerInterface $em): Response
+    {
+        $wish = new Wish();
+        $form = $this->createForm(WishType::class,$wish);
+        $form->handleRequest($req);
+
+        if ($form->isSubmitted()) {
+            $em->persist($wish);
+            $em->flush();
+            return $this->redirectToRoute("wishlist");
+        } else {
+            return $this->render('wish/add.html.twig',[
+                "form" => $form->createView()
+            ]);
+        }
     }
 }
